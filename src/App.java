f9c0a22ae607443cc2e9a8db2605880e0d4666a1
Guillermo <memorosas.00@gmail.com>
2024-06-18 15:58:11 -0600

    Primer comit app hola mundo

---
+++ src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hola mundo! Massivo Buaaap"); //Esto se imprime en la
+        // consola
+
+        System.out.println("Esto se ejecuta al inicio");
+        // coemtario
+
+        /*
+         * Comentarios massivos
+         * 
+         * 
+         */
+
+        {
+            // dentro
+            System.out.
+            println("hola dentro");  //dar un enter en medio de la sentencia no perjudica //quitar el punto y coma si es error
+        }
+
+    }
+}
